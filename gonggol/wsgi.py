0a45bfd19fb65f4e7940c11c91884c85d8b95408
최승리 <apol.lun058@gmail.com>
2021-06-07 16:59:44 +0900

    초기설정 수정

--- gonggol/wsgi.py
+++ gonggol/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for kkangol project.
+WSGI config for gonggol project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kkangol.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gonggol.settings')
 
 application = get_wsgi_application()
