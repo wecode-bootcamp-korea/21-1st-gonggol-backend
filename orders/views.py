f6e3dd9d5612be4dcad8635f4e0562d4f53edbd5
최승리 <apol.lun058@gmail.com>
2021-06-10 10:30:41 +0900

    Add:order_start

--- orders/views.py
+++ orders/views.py
@@ -3,9 +3,15 @@ import json
 from django.http.response import JsonResponse
 from django.http import JsonResponse
 from django.views import View
+from users.models import User
 
 
 class CartList(View):
     def get(self, request):
         return JsonResponse({"a":"b"}, status=200)
-
+    
+    def post(self, request):
+        data = json.load(request.body)
+        user = data['account']
+        product = data['product']
+        quantity = data['quantity']
