415fc2cbc813a25f5a423f62cb1b80fb22c9db2f
cj4207 <cksdud1226@gmail.com>
2021-06-15 16:27:07 +0900

    Refactor : 충돌 수정

--- products/urls.py
+++ products/urls.py
@@ -1,15 +1,8 @@
 from django.urls import path
-<<<<<<< HEAD
-from products.views import ProductMainView
 
-urlpatterns = [
-    path('', ProductMainView.as_view())
-]
-=======
-
-from products.views import ProductDetailView
+from products.views import ProductDetailView, ProductMainView
 
 urlpatterns = [
     path('/<product_id>', ProductDetailView.as_view()),
+    path('', ProductMainView.as_view())
 ]
->>>>>>> main
