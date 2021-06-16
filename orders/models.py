1a8f8b53f438f830ba9a6921175be5a76740a9a1
최승리 <apol.lun058@gmail.com>
2021-06-16 17:17:55 +0900

    [최승리]:cart_remodeling

--- orders/models.py
+++ orders/models.py
@@ -3,11 +3,26 @@ from django.db.models.deletion import CASCADE
 from django.db.models.fields import CharField
 from django.db.models.fields.related import ForeignKey
 
-class Order(models.Model):
-    user     = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    product  = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+class Cart(models.Model):
+    user       = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    status     = models.ForeignKey('Status', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'carts'
+
+class CartItem(models.Model):
+    cart     = models.ForeignKey('Cart', on_delete=CASCADE)
+    product  = models.ForeignKey('products.Product', on_delete=CASCADE)
     quantity = models.IntegerField(default=1)
     size     = models.CharField(max_length=10, null=True)
 
     class Meta:
-        db_table = 'orders'
+        db_table = 'cartitems'
+
+class Status(models.Model):
+    status_name = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'status'
--- orders/views.py
+++ orders/views.py
@@ -3,44 +3,47 @@ import json
 from django.views import View
 from django.http  import JsonResponse
 
-from .models         import Order
-from users.models    import User
-from products.models import Product, Image
+from .models         import Cart, CartItem, Status
+from products.models import Product
 from users.utils     import UserInfoDeco
 
 class CartView(View):
     @UserInfoDeco
-    def post(self, request): # 제품상세에서 장바구니 눌렀을 때, 우선 DB에 저장
-        try:
-            user     = request.user # token 유저정보
-            data     = json.loads(request.body)
-            product  = Product.objects.get(id=data['productId'])
-            size     = data['size']
-            quantity = data['quantity']
+    def post(self, request):
+        user     = request.user
+        data     = json.loads(request.body)
+        product  = Product.objects.get(id=data['productId'])
+        size     = data['size']
+        quantity = data['quantity']
+        status   = Status.objects.get(id=1)
+        
+        if not Cart.objects.get(user_id=user.id, status=status): # 결제대기중인 해당유정의 cart테이블 찾기
+            Cart.objects.create(user_id=user.id, status=status)  # 없다면 생성
 
-            if Order.objects.filter(user_id=user.id, product_id=product.id, size=size).exists(): # 장바구니에 이미 같은 제품이 들어가있다면 갯수만 추가
-                user_order           = Order.objects.get(user_id=user.id, product_id=product.id, size=size)
-                user_order.quantity += int(quantity)
-                user_order.save()
-                return JsonResponse({"message":"장바구니 업데이트 성공"}, status=200)
-            else: # 아니라면 장바구니 생성
-                Order.objects.create(
-                    user_id    = user.id,
-                    product_id = product.id,
-                    size       = size,
-                    quantity   = quantity
-                    )
-                return JsonResponse({"message":"장바구니 생성 성공"}, status=200)
-        except Product.DoesNotExist:
-            return JsonResponse({"message":"잘못된 product"}, status=500)
+        cart = Cart.objects.get(user_id=user.id, status=status)
+
+        if CartItem.objects.filter(cart_id=cart.id, product_id=product.id, size=size).exists(): # 이미 CartItem에 동일 제품이 있다면 갯추만 증가
+            user_order           = CartItem.objects.get(cart_id=cart.id, product_id=product.id, size=size)
+            user_order.quantity += int(quantity)
+            user_order.save()
+            return JsonResponse({"message":"cart update success"}, status=200)
+        else: # 아니라면 CartItem 생성
+            CartItem.objects.create(
+                cart_id    = cart.id,
+                product_id = product.id,
+                size       = size,
+                quantity   = quantity
+                )
+            return JsonResponse({"message":"cart add success"}, status=200)
 
     @UserInfoDeco
     def get(self, request): # User의 장바구니 프론트단 전달
         user       = request.user
-        order_list = Order.objects.filter(user_id=user.id)
+        cart       = Cart.objects.get(user_id=user.id)
+        cart_items  = cart.cartitem_set.filter(cart_id=cart.id)
         
-        cart = []
-        for product_info in order_list:
+        cartlist = []
+        for product_info in cart_items:
             product = Product.objects.get(id=product_info.product_id)
             order_info = {
                 "product_name" : product.name,
@@ -49,6 +52,17 @@ class CartView(View):
                 "price"        : product.price,
                 "image"        : product.image_set.get(product_id=product.id).url
             }
-            cart.append(order_info)
-        return JsonResponse({"message" : cart}, status=200)
-            
+            cartlist.append(order_info)
+        return JsonResponse({"message" : cartlist}, status=200)
+
+    @UserInfoDeco
+    def delete(self, request): # 프론트로부터 입력받은 유저의 특정 제품, 장바구니에서 삭제
+        user    = request.user
+        data    = json.loads(request.body)
+        product = data["productId"]
+
+        user_info = Cart.objects.get(user_id=user.id)
+        del_item  = user_info.cartitem_set.filter(product_id = product)
+        del_item.delete()
+        return JsonResponse({"message" : "success"}, status=200)
+
